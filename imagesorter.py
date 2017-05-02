643f3477bc1b86211f7a37c7d88844f4c7555295
Erik Hallmark <erikhallmarkdev@gmail.com>
2017-05-01 22:11:55 -0700

    Added progress bar

--- imagesorter.py
+++ imagesorter.py
@@ -2,7 +2,7 @@
 
 from PIL import Image
 from optparse import OptionParser
-import os, re, shutil
+import os, re, shutil, sys
 
 parser = OptionParser("Usage: %prog source-directory [options]")
 filepattern = re.compile("([^\s]+(\.(jpeg|jpg|png|gif|bmp))$)", re.IGNORECASE)
@@ -13,12 +13,33 @@ parser.add_option("-s", "--square", dest="square_dir", help="Directory to add sq
 
 (options, args) = parser.parse_args()
 
-for f in os.listdir(os.path.abspath(args[0])):
+i = 0
+
+files = os.listdir(os.path.abspath(args[0]))
+
+#Credit for the progess bar goes to https://gist.github.com/vladignatyev/06860ec2040cb497f0f3
+
+def progress(count, total, status=''):
+  bar_len = 60
+  filled_len = int(round(bar_len * count / float(total)))
+
+  percents = round(100.0 * count / float(total), 1)
+  bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+  sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+  sys.stdout.flush() 
+
+
+for f in files:
   if(not filepattern.match(f)):
     continue
 
   image = Image.open(os.path.abspath(args[0]) + "/" + f)
   width,height = image.size
+
+  progress(i, len(files), f)
+
+  i = i + 1
 
   if(width > height): #landscape
     if(options.landscape_dir is not None):
@@ -32,3 +53,5 @@ for f in os.listdir(os.path.abspath(args[0])):
   if(width == height): #square
     if(options.squre_dir is not None):
       shutil.copy(os.path.abspath(args[0]) + "/" + f, os.path.abspath(options.square))
+
+print('')
